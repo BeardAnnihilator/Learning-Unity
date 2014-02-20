55b540d9ff07e28a33e210c2acf84a5689e589dd
BeardAnnihilator <alexandre.bigmoustache@gmail.com>
2014-02-20 23:20:48 +0100

    New: Runner : followed Tutorial until <Description>
    We'll add a variable to Runner so we can configure its jump velocity.
    We'll use a vector instead of just a float so we can profide both a
    vertical and horizontal component. Set the corresponding field in the
    editor to (1, 7, 0).
    We want Runner to jump only when it's touching a platform while the jump
    button is pressed. Let's add code for this to the Update method.

--- Runner-FirstTutorial/Assets/Platform/PlatformManager.cs
+++ Runner-FirstTutorial/Assets/Platform/PlatformManager.cs
@@ -1,35 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class SkylineManager : MonoBehaviour {
+public class PlatformManager : MonoBehaviour {
 	
 	public Transform prefab;
 	public int numberOfObjects;
 	public float recycleOffset;
 	public Vector3 startPosition;
-
-
+	public Vector3 minSize, maxSize, minGap, maxGap;
+	public float minY, maxY;
+	
 	private Vector3 nextPosition;
 	private Queue<Transform> objectQueue;
 	
 	void Start () {
 		objectQueue = new Queue<Transform>(numberOfObjects);
-		for (int i = 0; i < numberOfObjects; i++) {
+		for(int i = 0; i < numberOfObjects; i++){
 			objectQueue.Enqueue((Transform)Instantiate(prefab));
 		}
 		nextPosition = startPosition;
-		for (int i = 0; i < numberOfObjects; i++) {
-			Recycle();
-		}
-	}
-
-	void Update () {
-		if (objectQueue.Peek().localPosition.x + recycleOffset < Runner.distanceTraveled) {
+		for(int i = 0; i < numberOfObjects; i++){
 			Recycle();
 		}
 	}
 	
-	public Vector3 minSize, maxSize;
+	void Update () {
+		if(objectQueue.Peek().localPosition.x + recycleOffset < Runner.distanceTraveled){
+			Recycle();
+		}
+	}
 	
 	private void Recycle () {
 		Vector3 scale = new Vector3(
@@ -44,7 +43,18 @@ public class SkylineManager : MonoBehaviour {
 		Transform o = objectQueue.Dequeue();
 		o.localScale = scale;
 		o.localPosition = position;
-		nextPosition.x += scale.x;
 		objectQueue.Enqueue(o);
+		
+		nextPosition += new Vector3(
+			Random.Range(minGap.x, maxGap.x) + scale.x,
+			Random.Range(minGap.y, maxGap.y),
+			Random.Range(minGap.z, maxGap.z));
+		
+		if(nextPosition.y < minY){
+			nextPosition.y = minY + maxGap.y;
+		}
+		else if(nextPosition.y > maxY){
+			nextPosition.y = maxY - maxGap.y;
+		}
 	}
 }
--- Runner-FirstTutorial/Assets/Runner/Runner.cs
+++ Runner-FirstTutorial/Assets/Runner/Runner.cs
@@ -1,11 +1,37 @@
 ﻿using UnityEngine;
 
 public class Runner : MonoBehaviour {
-
+	
 	public static float distanceTraveled;
-
+	
+	public float acceleration;
+	
+	private bool touchingPlatform;
+	
+	public Vector3 jumpVelocity;
+	public Vector3 jumpBackVelocity;
+	
 	void Update () {
-		transform.Translate(5f * Time.deltaTime, 0f, 0f);
+		if(touchingPlatform && Input.GetButtonDown("Jump")){
+			rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);
+		}
+		else if(touchingPlatform && Input.GetButtonDown("JumpBack")){
+			rigidbody.AddForce(jumpBackVelocity, ForceMode.VelocityChange);
+		}
 		distanceTraveled = transform.localPosition.x;
+	}
+	
+	void FixedUpdate () {
+		if(touchingPlatform){
+			rigidbody.AddForce(acceleration, 0f, 0f, ForceMode.Acceleration);
+		}
+	}
+	
+	void OnCollisionEnter () {
+		touchingPlatform = true;
+	}
+	
+	void OnCollisionExit () {
+		touchingPlatform = false;
 	}
 }
